8fd100df87ca3d23142adc2065b24bee9a75bded
Unknown <mjsterli@gmail.com>
2017-02-07 10:16:37 -0600

    Adding Controller

---
+++ people-api/src/Love/Test/PeopleBundle/Controller/People.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Love\Test\PeopleBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use PeopleBundle\Entity\Person;
